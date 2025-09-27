594b1246b528e91ceca8194684a78a410808bea6
Tayyab Jadoon <tayyabkhan5228131@gmail.com>
2025-09-27 08:43:05 +0500

    add textarea and edit controls to Todo item structure

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
 const itemsArray=localStorage.getItem("items")?JSON.parse(localStorage.getItem("items")):[];
-console.log(itemsArray);
+
 document.querySelector("#enter").addEventListener("click",()=>{
     const item=document.querySelector("#item")
     createItem(item)
 })
+function displayItems(){
+    let items="";
+    for(let i=0;i<itemsArray.length;i++){
+        
+    }
+}
 function createItem(item){
     itemsArray.push(item.value)
     localStorage.setItem("items",JSON.stringify(itemsArray))
+    location.reload()
 }
-
+//Date display
 function displayDate(){
     let date=new Date()
     date=date.toString().split(" ")
